d950fa66b122660f18cce9c53ea7b56e39c27e00
조희우 <huiwoo.jo@gmail.com>
2026-01-12 12:38:29 +0900

    🎨 add emotion and global styles

---
+++ src/styles/themes.ts
@@ -0,0 +1,31 @@
+export const COLOR = {
+    GRAY700: '#303239',
+    GRAY600: '#50525F',
+    GRAY500: '#7F8295',
+    GRAY400: '#AEB2C6',
+    GRAY300: '#D0D3E1',
+    GRAY200: '#E4E5ED',
+    GRAY100: '#F1F2F6',
+    WHITE: '#FFFFFF',
+    BLACK: '#000000',
+    BLUE500: '#509CF7',
+    BLUE400: '#79B4F9',
+    BLUE300: '#AED1FB',
+    BLUE200: '#CEE7F7',
+    PINK600: '#FD5B73',
+    PINK500: '#FF8093',
+    FILTER: 'rgba(127, 130, 149, 0.30)',
+    TOAST: 'rgba(0, 0, 0, 0.80)',
+  } as const;
+  
+  export const FONT = {
+    // 추후 추가
+  } as const;
+  
+  export const theme = {
+    COLOR,
+    FONT,
+  } as const;
+  
+  export type ColorType = typeof COLOR;
+  export type FontType = typeof FONT;  
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		svgr({
+			svgrOptions: {
+				icon: true, // 아이콘을 위한 설정
+			},
+		}),
+	],
+	resolve: {
+		alias: [
+			{ find: '@', replacement: '/src' },
+			{ find: '@components', replacement: '/src/components' },
+			{ find: '@pages', replacement: '/src/pages' },
+			{ find: '@assets', replacement: '/src/assets' },
+			{ find: '@const', replacement: '/src/const' },
+			{ find: '@store', replacement: '/src/store' },
+			{ find: '@styles', replacement: '/src/styles' },
+			{ find: '@types', replacement: '/src/types' },
+			{ find: '@utils', replacement: '/src/utils' },
+		],
+	},
+	server: {
+		port: 3000,
+	},
+});
