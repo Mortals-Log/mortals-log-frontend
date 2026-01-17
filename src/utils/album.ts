5ca73d6838a7069366cb54625a430d2db857b71a
조희우 <huiwoo.jo@gmail.com>
2026-01-17 13:08:35 +0900

    🔧 prevent runtime errors when the album list is empty

--- src/utils/album.ts
+++ src/utils/album.ts
@@ -3,7 +3,8 @@
 
 import { Album } from '@/types/album';
 
-export const GetLatestAlbum = (albumList: Album[]): Album => {
+export const GetLatestAlbum = (albumList: Album[]): Album | undefined => {
+	if (albumList.length === 0) return undefined;
 	return [...albumList].sort(
 		(a, b) =>
 			new Date(b.releaseDate.replace(/\./g, '-')).getTime() - new Date(a.releaseDate.replace(/\./g, '-')).getTime(),
