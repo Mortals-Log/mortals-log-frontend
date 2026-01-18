dc848445da66eb0c4ca4a086b91450e73be01b7a
조희우 <huiwoo.jo@gmail.com>
2026-01-18 16:35:51 +0900

    🐛 removing elvis operators from the Links interface to ensure data integrity when mapping items in the GNB

--- src/types/links.ts
+++ src/types/links.ts
@@ -2,13 +2,13 @@
 /* eslint-disable storybook/default-exports */
 
 export interface Links {
-	INSTAGRAM?: string;
-	YOUTUBE?: string;
-	SPOTIFY?: string;
-	APPLE_MUSIC?: string;
-	GOODS_CD?: string;
-	GOODS_LP?: string;
-	GOODS_SHOP?: string;
-	OPEN_CHAT?: string;
-	TJ_SONG_REQUEST?: string;
+	INSTAGRAM: string;
+	YOUTUBE: string;
+	SPOTIFY: string;
+	APPLE_MUSIC: string;
+	GOODS_CD: string;
+	GOODS_LP: string;
+	GOODS_SHOP: string;
+	OPEN_CHAT: string;
+	TJ_SONG_REQUEST: string;
 }
