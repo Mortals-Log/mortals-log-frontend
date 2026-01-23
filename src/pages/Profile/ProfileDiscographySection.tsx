36eb16855bdc51b8d83b606d94cd1517bde2a067
조희우 <huiwoo.jo@gmail.com>
2026-01-23 13:55:47 +0900

    🎨 separate AlbumCard and Slider styles

--- src/pages/Profile/ProfileDiscographySection.tsx
+++ src/pages/Profile/ProfileDiscographySection.tsx
@@ -47,13 +47,13 @@ const DiscographySection = ({ TITLE_KR, TITLE_EN }: { TITLE_KR: string; TITLE_EN
 				{TITLE_KR} <span>{TITLE_EN}</span>
 			</S.SectionTitle>
 
-			<S.AlbumSliderContainer>
+			<S.SliderContainer>
 				{!isAtStart && (
 					<S.SliderNavButton $direction="left" onClick={() => handleScroll('left')}>
 						<span>‹</span>
 					</S.SliderNavButton>
 				)}
-				<S.AlbumSlider ref={sliderRef} onScroll={checkScrollPosition}>
+				<S.Slider ref={sliderRef} onScroll={checkScrollPosition}>
 					{LP_ALBUMS.map(album => (
 						<S.AlbumCard key={`${album.type}-${album.title}`}>
 							<img
@@ -78,14 +78,14 @@ const DiscographySection = ({ TITLE_KR, TITLE_EN }: { TITLE_KR: string; TITLE_EN
 							</S.AlbumInfo>
 						</S.AlbumCard>
 					))}
-				</S.AlbumSlider>
+				</S.Slider>
 
 				{!isAtEnd && (
 					<S.SliderNavButton $direction="right" onClick={() => handleScroll('right')}>
 						<span>›</span>
 					</S.SliderNavButton>
 				)}
-			</S.AlbumSliderContainer>
+			</S.SliderContainer>
 
 			<S.MoreButton to="/album" target="_self" rel="noreferrer">
 				전체 앨범 보러가기
--- src/styles/pages/Profile/index.ts
+++ src/styles/pages/Profile/index.ts
@@ -2,9 +2,12 @@
 
 /* eslint-disable storybook/default-exports */
 
-export * from '@styles/components/Buttons.style';
 export * from '@styles/pages/Profile/Profile.style';
-export * from '@styles/common/SectionTitle';
 
+export * from '@styles/components/Buttons.style';
+export * from '@styles/components/AlbumCard';
+export * from '@styles/components/Slider';
+
+export * from '@styles/common/SectionTitle';
 export * from '@styles/common/ArrowIcon';
 export * from '@styles/common/ExternalIcon';
