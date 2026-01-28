a344ab630e3fe69f50481e06454d2fd2fe28e160
조희우 <huiwoo.jo@gmail.com>
2026-01-28 14:36:54 +0900

    💄edit the description of album page

--- src/pages/Album/index.tsx
+++ src/pages/Album/index.tsx
@@ -9,7 +9,7 @@ import AlbumPromotionSection from './AlbumPromotionSection';
 const PAGE_TITLE = {
 	MAIN: '앨범',
 	SUB: 'ALBUM',
-	DESCRIPTION: `${NAME.KOREAN}의 정규 앨범부터 싱글, 라이브까지 각 앨범의 상세 정보와 수록곡의 가사와 코드를 확인할 수 있습니다.`,
+	DESCRIPTION: `${NAME.KOREAN}의 정규 앨범부터 싱글, 라이브까지 각 앨범의 상세 정보와 수록곡을 확인할 수 있습니다.`,
 } as const;
 
 const SECTION_TITLE = {
