bdbeddc0ec99447de804909a67d84009907392b6
조희우 <huiwoo.jo@gmail.com>
2026-02-02 19:32:21 +0900

    ♻️ extract platform constants and refactor link data

--- src/const/links.ts
+++ src/const/links.ts
@@ -4,13 +4,35 @@
 
 import { Links, LinkDetail } from '@/types/links';
 
+export const MUSIC_PLATFORM = {
+	APPLE_MUSIC: 'Apple Music',
+	SPOTIFY: 'Spotify',
+	YOUTUBE: 'YouTube',
+	SOUNDCLOUD: 'Sound Cloud',
+} as const;
+
+export const SNS_PLATFORM = {
+	INSTAGRAM: 'Instagram',
+	KAKAOTALK: 'Kakaotalk',
+} as const;
+
+export const SHOP_PLATFORM = {
+	CD: 'CD',
+	LP: 'LP',
+	GOODS: 'Goods',
+} as const;
+
+export const ETC_PLATFORM = {
+	LINK_TREE: 'Link Tree',
+} as const;
+
 export const LINK_PLATFORM = {
 	INSTAGRAM: {
-		NAME: 'instagram',
+		NAME: SNS_PLATFORM.INSTAGRAM,
 		BASE_URL: 'https://www.instagram.com/p/',
 	},
 	YOUTUBE: {
-		NAME: 'youtube',
+		NAME: MUSIC_PLATFORM.YOUTUBE,
 		BASE_URL: 'https://www.youtube.com/watch?v=',
 	},
 } as const;
@@ -34,36 +56,39 @@ export const LINK_LIST: Links = [
 	{
 		category: 'SNS',
 		items: [
-			{ label: 'Instagram', url: 'https://www.instagram.com/1000_jinwoo' },
+			{ label: SNS_PLATFORM.INSTAGRAM, url: 'https://www.instagram.com/1000_jinwoo' },
 
-			{ label: 'Kakaotalk', url: 'https://open.kakao.com/o/gj0SK2Qe' },
+			{ label: SNS_PLATFORM.KAKAOTALK, url: 'https://open.kakao.com/o/gj0SK2Qe' },
 		],
 	},
 	{
 		category: 'MUSIC',
 		items: [
-			{ label: 'Apple Music', url: 'https://music.apple.com/kr/artist/%EC%B2%9C%EC%A7%84%EC%9A%B0/1641616138?ls' },
-			{ label: 'Spotify', url: 'https://open.spotify.com/artist/5OEgs7d2znP7y50pp8y7bK' },
-			{ label: 'YouTube', url: 'https://www.youtube.com/channel/UCnjNCmBszWrgEO0NvfAbbVQ' },
-			{ label: 'Sound Cloud', url: 'https://soundcloud.com/1000_jinwoo' },
+			{
+				label: MUSIC_PLATFORM.APPLE_MUSIC,
+				url: 'https://music.apple.com/kr/artist/%EC%B2%9C%EC%A7%84%EC%9A%B0/1641616138?ls',
+			},
+			{ label: MUSIC_PLATFORM.SPOTIFY, url: 'https://open.spotify.com/artist/5OEgs7d2znP7y50pp8y7bK' },
+			{ label: MUSIC_PLATFORM.YOUTUBE, url: 'https://www.youtube.com/channel/UCnjNCmBszWrgEO0NvfAbbVQ' },
+			{ label: MUSIC_PLATFORM.SOUNDCLOUD, url: 'https://soundcloud.com/1000_jinwoo' },
 		],
 	},
 	{
 		category: 'SHOP',
 		items: [
 			{
-				label: 'CD',
+				label: SHOP_PLATFORM.CD,
 				url: 'https://gimbabrecords.com/product/search.html?view_type=&supplier_code=&category_no=&keyword=천진우&x=0&y=0',
 			},
-			{ label: 'LP', url: 'https://smartstore.naver.com/irrelevant/products/10239470111' },
-			{ label: 'Goods', url: 'https://marpple.shop/kr/1000_jinwoo' },
+			{ label: SHOP_PLATFORM.LP, url: 'https://smartstore.naver.com/irrelevant/products/10239470111' },
+			{ label: SHOP_PLATFORM.GOODS, url: 'https://marpple.shop/kr/1000_jinwoo' },
 		],
 	},
 	{
 		category: 'ETC',
 		items: [
 			{
-				label: 'Link Tree',
+				label: ETC_PLATFORM.LINK_TREE,
 				url: 'https://linktr.ee/1000jinwoo?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGn9XKUd5Bfg5-nWM88QQh643PrJrleX7QfjSieYPTZeUJc6nT11qYg3vGO8So_aem_3Du4C9eWuHq6MrdFtTKmDA',
 			},
 			{ label: 'Sing', url: 'https://www.tjmedia.com/song/accompaniment_apply_agree' },
