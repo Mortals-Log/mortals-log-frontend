a556953a57d6c08f50e9bcc8c091ebfea9c6bdeb
조희우 <huiwoo.jo@gmail.com>
2026-02-04 20:16:49 +0900

    ✨ modified to allow direct setting of message in the placeholder

--- src/components/placeholder.tsx
+++ src/components/placeholder.tsx
@@ -2,8 +2,15 @@
 
 import * as S from '@styles/components/Placeholder.style';
 
-const Placeholder = ({ contentName }: { contentName: string }) => {
-	return <S.Placeholder>{contentName}은(는) 데이터 준비 후 업데이트될 예정입니다.</S.Placeholder>;
+interface PlaceholderProps {
+	contentName?: string;
+	message?: string;
+}
+
+const Placeholder = ({ contentName, message }: PlaceholderProps) => {
+	return (
+		<S.Placeholder>{message ? message : `${contentName}은(는) 데이터 준비 후 업데이트될 예정입니다.`}</S.Placeholder>
+	);
 };
 
 export default Placeholder;
