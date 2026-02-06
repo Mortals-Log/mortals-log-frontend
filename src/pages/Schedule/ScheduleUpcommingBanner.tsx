1236daadc363c93cb9cce3b17cbd49342d6e76c5
조희우 <huiwoo.jo@gmail.com>
2026-02-06 09:42:16 +0900

    ✨ add upcomming schedule banner in schedule page

---
+++ src/pages/Schedule/ScheduleUpcommingBanner.tsx
@@ -0,0 +1,55 @@
+// @components/Schedule/UpcomingBanner.tsx
+
+import { useMemo } from 'react';
+import * as S from '@styles/pages/Schedule/ScheduleUpcommingBanner.style';
+import { CALENDAR_SCHEDULES } from '@utils/schedule';
+import { addDays, differenceInDays, format, isWithinInterval, parseISO, startOfDay } from 'date-fns';
+import { SCHEDULE_LABEL_MAP } from '@/const/schedule';
+
+const ScheduleUpcommingBacnner = ({ TITLE_KR, TITLE_EN }: { TITLE_KR: string; TITLE_EN: string }) => {
+	const today = startOfDay(new Date());
+	const limitDay = addDays(today, 7);
+
+	const upcomingEvents = useMemo(() => {
+		return Object.entries(CALENDAR_SCHEDULES)
+			.filter(([dateStr]) => {
+				const date = parseISO(dateStr);
+				return isWithinInterval(date, { start: today, end: limitDay });
+			})
+			.flatMap(([dateStr, events]) =>
+				events.map(event => {
+					const eventDate = parseISO(dateStr);
+					const diff = differenceInDays(eventDate, today);
+					const dDayLabel = `D-${diff === 0 ? 'DAY' : diff}`;
+
+					return { ...event, date: dateStr, dDayLabel, diff };
+				}),
+			)
+			.sort((a, b) => a.date.localeCompare(b.date));
+	}, [today, limitDay]);
+
+	if (upcomingEvents.length === 0) return null;
+
+	return (
+		<S.ContentSection>
+			<S.SectionTitle>
+				{TITLE_KR} <span>{TITLE_EN}</span>
+			</S.SectionTitle>
+
+			<S.SliderContainer>
+				{upcomingEvents.map((event, idx) => (
+					<S.BannerItem key={idx} eventType={event.type} dDay={event.diff}>
+						<div className="info">
+							<span className="dDay">{event.dDayLabel}</span>
+							<span className="date">{format(parseISO(event.date), 'MM.dd')}</span>
+							<span className="type">{SCHEDULE_LABEL_MAP[event.type]}</span>
+							<p className="content">{event.content}</p>
+						</div>
+					</S.BannerItem>
+				))}
+			</S.SliderContainer>
+		</S.ContentSection>
+	);
+};
+
+export default ScheduleUpcommingBacnner;
---
+++ src/styles/pages/Schedule/ScheduleUpcommingBanner.style.ts
@@ -0,0 +1,77 @@
+// @styles/pages/Schedule/ScheduleUpcommingBanner.style.ts
+
+/* eslint-disable storybook/default-exports */
+
+import styled from '@emotion/styled';
+import { Schedule } from '@/types/schedule';
+import { SCHEDULE_TYPE_COLORS } from '@/const/schedule';
+
+export * from '@styles/pages/Schedule/Schedule.style';
+
+export const SliderContainer = styled.div`
+	display: flex;
+	overflow-x: auto;
+	gap: 12px;
+	margin-top: 1rem;
+
+	scroll-snap-type: x mandatory;
+	scroll-behavior: smooth;
+	-webkit-overflow-scrolling: touch;
+	&::-webkit-scrollbar {
+		display: none;
+	}
+`;
+
+export const BannerItem = styled.div<{ eventType: Schedule['type']; dDay: number }>`
+	display: flex;
+	position: relative;
+	width: 20rem;
+	min-height: 6rem;
+	padding: 1.5rem 1rem;
+
+	align-items: center;
+	scroll-snap-align: start;
+	flex-shrink: 0;
+
+	border-radius: 8px;
+	border: 1.3px solid ${props => (props.dDay == 0 ? props.theme.COLOR.PRIMARY : props.theme.COLOR.GRAY100)};
+	transition: all 0.15s ease-in-out;
+	cursor: pointer;
+
+	font-family: ${props => props.theme.FONT.SANS};
+
+	.info {
+		.dDay {
+			font-size: ${props => props.theme.FONT.SIZE.MD};
+			font-weight: ${props => props.theme.FONT.WEIGHT.SEMIBOLD};
+			color: ${props => props.theme.COLOR.PRIMARY};
+		}
+		.date {
+			margin-left: 0.5rem;
+			text-transform: uppercase;
+			font-size: ${props => props.theme.FONT.SIZE.SM};
+			font-weight: ${props => props.theme.FONT.WEIGHT.MEDIUM};
+			color: ${props => props.theme.COLOR.GRAY600};
+		}
+		.type {
+			margin-left: 0.5rem;
+			font-size: ${props => props.theme.FONT.SIZE.XS};
+			font-weight: ${props => props.theme.FONT.WEIGHT.MEDIUM};
+			color: ${props => props.theme.COLOR.GRAY500};
+		}
+		.content {
+			font-size: ${props => props.theme.FONT.SIZE.SM};
+			font-weight: ${props => props.theme.FONT.WEIGHT.REGULAR};
+			color: ${props => props.theme.COLOR.GRAY700};
+			margin-top: 0.3rem;
+			gap: 2px;
+			line-height: 1.4;
+			letter-spacing: -0.01em;
+		}
+	}
+
+	&:hover {
+		border: 1.3px solid ${props => SCHEDULE_TYPE_COLORS[props.eventType].text + '33'};
+		background-color: ${props => SCHEDULE_TYPE_COLORS[props.eventType].bg + '66'};
+	}
+`;
