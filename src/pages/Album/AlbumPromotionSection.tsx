d3f948083216dcb2f0a797184848b496e8573de2
조희우 <huiwoo.jo@gmail.com>
2026-02-07 09:57:15 +0900

    ✨ add feature for upcoming album in album promotion section on album page

--- src/pages/Album/AlbumPromotionSection.tsx
+++ src/pages/Album/AlbumPromotionSection.tsx
@@ -5,7 +5,9 @@ import { ALBUM_TYPE_LABEL, FULL_ALBUMS } from '@const/albums';
 import { MASTER_TRACKS } from '@const/tracks';
 import { GetLatestAlbum } from '@utils/album';
 import { GetTracks } from '@utils/track';
+import { differenceInDays, format, parse, startOfDay } from 'date-fns';
 
+const today = startOfDay(new Date());
 const album = GetLatestAlbum(FULL_ALBUMS);
 
 const AlbumPromotionSection = () => {
@@ -18,41 +20,70 @@ const AlbumPromotionSection = () => {
 	const allTrackIds = GetTracks(album.tracks || []);
 	const previewTrackIds = allTrackIds.slice(0, 3);
 
+	const parseDate = parse(album.releaseDate, 'yyyy.MM.dd', new Date());
+	const releaseDate = format(parseDate, 'yyyy.MM.dd');
+	const diff = differenceInDays(parseDate, today);
+	const isReleased = diff < 0;
+
+	const handleImgError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+		e.currentTarget.src = '/images/albums/default.webp';
+	};
+
 	return (
 		<S.ContentSection>
 			<S.ContentWrapper>
+				{!isReleased && <S.DDayBadge>{diff === 0 ? 'D-Day' : `D-${diff}`}</S.DDayBadge>}
+
 				<S.ImageArea>
-					<S.CoverImage src={`/images/albums/${key}.webp`} alt={album.title} />
+					<S.CoverImage src={`/images/albums/${key}.webp`} alt={album.title} onError={handleImgError} />
 				</S.ImageArea>
 
 				<S.InfoArea>
-					<S.Tag>⊹ LATEST RELEASE ⊹</S.Tag>
+					<S.Tag>{isReleased ? '⊹ LATEST RELEASE ⊹' : '⊹ UPCOMING RELEASE ⊹'}</S.Tag>
 					<S.Title>{album.title}</S.Title>
 					<S.Info>
-						{ALBUM_TYPE_LABEL[album.type]} • {album.releaseDate}
+						{ALBUM_TYPE_LABEL[album.type]} • {releaseDate}
 					</S.Info>
-					<S.Description>새로운 이야기가 담긴 {album.title}을 지금 만나보세요.</S.Description>
 
-					{previewTrackIds.length > 0 && (
-						<S.TrackPreviewList>
-							{previewTrackIds.map((trackId, index) => {
-								const track = MASTER_TRACKS[trackId];
+					<S.Description>
+						{isReleased
+							? `새로운 이야기가 담긴 ${album.title}을 지금 만나보세요.`
+							: `${album.title}의 새로운 시작을 준비하세요.`}
+					</S.Description>
 
-								return (
-									<S.TrackItem key={trackId}>
-										<span className="number">{String(index + 1).padStart(2, '0')}</span>
-										<span className="name">{track.title}</span>
-									</S.TrackItem>
-								);
-							})}
+					<S.TrackPreviewList>
+						{isReleased ? (
+							previewTrackIds.length > 0 ? (
+								<>
+									{previewTrackIds.map((trackId, index) => {
+										const track = MASTER_TRACKS[trackId];
+										return (
+											<S.TrackItem key={trackId}>
+												<span className="number">{String(index + 1).padStart(2, '0')}</span>
+												<span className="name">{track?.title}</span>
+											</S.TrackItem>
+										);
+									})}
 
-							{allTrackIds.length > previewTrackIds.length && (
-								<S.MoreText>외 {allTrackIds.length - previewTrackIds.length}곡을 만나보세요.</S.MoreText>
-							)}
-						</S.TrackPreviewList>
+									{allTrackIds.length > previewTrackIds.length && (
+										<S.MoreText>외 {allTrackIds.length - previewTrackIds.length}곡을 만나보세요.</S.MoreText>
+									)}
+								</>
+							) : (
+								<S.TrackItem>트랙 정보가 없습니다.</S.TrackItem>
+							)
+						) : (
+							<>
+								<S.TrackItem>Coming Soon ...</S.TrackItem>
+							</>
+						)}
+					</S.TrackPreviewList>
+
+					{isReleased ? (
+						<S.PromotionLinkButton to={`/album/${encodeURIComponent(slug)}`}>VIEW TRACKS</S.PromotionLinkButton>
+					) : (
+						<S.PromotionLinkButton to={`/album/${encodeURIComponent(slug)}`}>MOVE TO PAGE</S.PromotionLinkButton>
 					)}
-
-					<S.PromotionLinkButton to={`/album/${encodeURIComponent(slug)}`}>VIEW TRACKS</S.PromotionLinkButton>
 				</S.InfoArea>
 			</S.ContentWrapper>
 		</S.ContentSection>
