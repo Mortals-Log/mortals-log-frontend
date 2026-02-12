f23548a140f43de8ea3aca97c895fed415cc3f56
조희우 <huiwoo.jo@gmail.com>
2026-02-12 11:59:16 +0900

    💄 add instagram link button in birthday and debut anniversary page

--- src/pages/Schedule/ScheduleDetailAnniversary.tsx
+++ src/pages/Schedule/ScheduleDetailAnniversary.tsx
@@ -4,12 +4,17 @@ import * as S from '@styles/pages/Schedule/ScheduleDetail.style';
 import { GetDay } from '@/utils/date';
 import { Schedule } from '@/types/schedule';
 import handleCopy from '@/hooks/handlecopy';
+import { LINK_LIST, SNS_PLATFORM } from '@/const/links';
 
 interface AnniversaryProps {
 	schedule: Schedule;
 }
 
 const ScheduleDetailAnniversary = ({ schedule }: AnniversaryProps) => {
+	const instagramUrl = LINK_LIST.find(links => links.category === 'SNS')?.items.find(
+		item => item.label === SNS_PLATFORM.INSTAGRAM,
+	)?.url;
+
 	// todo: song 데이터들을 추가 후 '굴다리 EP'를 불러오는 방식으로 수정 필요
 	const debutSong = 'https://www.youtube.com/embed/K9aIiynSPU4?si=Hk2joxHVnKDnis3C';
 
@@ -53,6 +58,14 @@ const ScheduleDetailAnniversary = ({ schedule }: AnniversaryProps) => {
 							</S.TagWrapper>
 
 							<span>* 태그를 클릭하면 복사됩니다.</span>
+						</S.InfoGroup>
+					)}
+
+					{instagramUrl && (
+						<S.InfoGroup>
+							<S.PrimaryButton href={instagramUrl} target="_blank" rel="noopener noreferrer">
+								{schedule.content} 축하하러 가기
+							</S.PrimaryButton>
 						</S.InfoGroup>
 					)}
 				</S.ContentSection>
--- src/pages/Schedule/ScheduleDetailBirthday.tsx
+++ src/pages/Schedule/ScheduleDetailBirthday.tsx
@@ -4,12 +4,17 @@ import * as S from '@styles/pages/Schedule/ScheduleDetail.style';
 import { GetDay } from '@/utils/date';
 import { Schedule } from '@/types/schedule';
 import handleCopy from '@/hooks/handlecopy';
+import { LINK_LIST, SNS_PLATFORM } from '@/const/links';
 
 interface BirthdayProps {
 	schedule: Schedule;
 }
 
 const ScheduleDetailBirthday = ({ schedule }: BirthdayProps) => {
+	const instagramUrl = LINK_LIST.find(links => links.category === 'SNS')?.items.find(
+		item => item.label === SNS_PLATFORM.INSTAGRAM,
+	)?.url;
+
 	// todo: song 데이터들을 추가 후 '생일축하해'를 불러오는 방식으로 수정 필요
 	const birthdaySong = 'https://www.youtube.com/embed/wHe8ntDlOco?si=q8jBY6HVplU9kTye';
 
@@ -53,6 +58,14 @@ const ScheduleDetailBirthday = ({ schedule }: BirthdayProps) => {
 							</S.TagWrapper>
 
 							<span>* 태그를 클릭하면 복사됩니다.</span>
+						</S.InfoGroup>
+					)}
+
+					{instagramUrl && (
+						<S.InfoGroup>
+							<S.PrimaryButton href={instagramUrl} target="_blank" rel="noopener noreferrer">
+								{schedule.content} 축하하러 가기
+							</S.PrimaryButton>
 						</S.InfoGroup>
 					)}
 				</S.ContentSection>
