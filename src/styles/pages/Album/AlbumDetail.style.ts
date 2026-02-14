1e707e7778a4ff2f7c8153447565a4fcb48984f9
조희우 <huiwoo.jo@gmail.com>
2026-02-14 11:11:47 +0900

    💄 change design and path of Album Detail page according to the changes in the Album

--- src/styles/pages/Album/AlbumDetail.style.ts
+++ src/styles/pages/Album/AlbumDetail.style.ts
@@ -3,10 +3,41 @@
 /* eslint-disable storybook/default-exports */
 
 import styled from '@emotion/styled';
-import { MainContainer as BaseContainer } from '@styles/pages/Album/Album.style';
 
 export * from '@styles/pages/Album/Album.style';
 export * from '@styles/components/Buttons.style';
-export const MainContainer = styled(BaseContainer)`
-	margin-top: -0.5rem;
+
+export const MainContainer = styled.main`
+	max-width: 800px;
+	margin: 0 auto;
+	padding: 100px 20px;
+`;
+
+export const MainTitle = styled.h2`
+	font-family: ${props => props.theme.FONT.SERIF};
+	font-size: ${props => props.theme.FONT.SIZE.H2};
+	font-weight: ${props => props.theme.FONT.WEIGHT.MEDIUM};
+	color: ${props => props.theme.COLOR.BLACK};
+	margin-top: 0.5rem;
+`;
+
+export const SubTitle = styled.span`
+	font-family: ${props => props.theme.FONT.SANS};
+	font-size: ${props => props.theme.FONT.SIZE.MD};
+	font-weight: ${props => props.theme.FONT.WEIGHT.REGULAR};
+	color: ${props => props.theme.COLOR.PRIMARY};
+	text-transform: uppercase;
+`;
+
+export const Description = styled.div`
+	margin-top: 1rem;
+	margin-bottom: -2rem;
+
+	font-family: ${props => props.theme.FONT.SANS};
+	font-size: ${props => props.theme.FONT.SIZE.SM};
+	font-weight: ${props => props.theme.FONT.WEIGHT.REGULAR};
+	color: ${props => props.theme.COLOR.GRAY500};
+
+	line-height: 1.5;
+	white-space: pre-wrap;
 `;
