a78308d0d065e57af8ffc145c72859dff6dc1ad1
조희우 <huiwoo.jo@gmail.com>
2026-02-21 11:46:48 +0900

    🐛 change badge format as chords become list

--- src/pages/Album/AlbumDetailTracks.tsx
+++ src/pages/Album/AlbumDetailTracks.tsx
@@ -83,7 +83,7 @@ const AlbumDetailTracks = ({
 										<S.TrackTitle $isLead={track.isLead || false}>
 											{track.title} {track.version && `(${track.version})`}
 											{track.isLead && <S.LeadBadge>TITLE</S.LeadBadge>}
-											{track.chords && <S.GuitarBadge>CHORDS</S.GuitarBadge>}
+											{track.chordsList && track.chordsList.length > 0 && <S.GuitarBadge>CHORDS</S.GuitarBadge>}
 											{track.ageLimit && <S.AdultBadge>🔞 미성년자 청취불가</S.AdultBadge>}
 											{track.singing && (
 												<>
--- src/pages/Song/index.tsx
+++ src/pages/Song/index.tsx
@@ -77,7 +77,7 @@ const Song = () => {
 								{track.title}
 								{track.version && ` ${track.version}`}
 								{track.isLead && <S.LeadBadge>TITLE</S.LeadBadge>}
-								{track.chords && <S.GuitarBadge>CHORDS</S.GuitarBadge>}
+								{track.chordsList && track.chordsList.length > 0 && <S.GuitarBadge>CHORDS</S.GuitarBadge>}
 								{track.ageLimit && <S.AdultBadge>🔞 미성년자 청취불가</S.AdultBadge>}
 								{track.singing && (
 									<>
