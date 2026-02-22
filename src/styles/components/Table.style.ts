82bae9b11986803c54538dd4ac36c6235ef3204e
조희우 <huiwoo.jo@gmail.com>
2026-02-23 08:03:28 +0900

    💄 change Table to suit mobile environment

--- src/styles/components/Table.style.ts
+++ src/styles/components/Table.style.ts
@@ -7,15 +7,14 @@ import styled from '@emotion/styled';
 export const Table = styled.table`
 	width: 100%;
 	border-collapse: collapse;
-	font-family: ${props => props.theme.FONT.SANS};
+	white-space: nowrap;
+	word-break: keep-all;
 
 	tr {
 		border-bottom: 1px solid ${props => props.theme.COLOR.GRAY300};
 	}
 
 	th {
-		font-size: ${props => props.theme.FONT.SIZE.MD};
-		font-weight: ${props => props.theme.FONT.WEIGHT.MEDIUM};
 		text-align: center;
 		vertical-align: middle;
 		padding: 16px 10px;
@@ -27,11 +26,29 @@ export const Table = styled.table`
 		vertical-align: middle;
 	}
 
-	@media (max-width: 480px) {
-		th,
-		td {
+	@media ${props => props.theme.WINDOW_SIZE.mobile} {
+		tr {
+			display: flex;
+			flex-direction: column;
+		}
+
+		th {
 			display: block;
+			width: fit-content;
+			border-bottom: 1px solid ${props => props.theme.COLOR.PRIMARY};
+
+			padding-bottom: 8px;
+
+			font-size: ${props => props.theme.FONT.SIZE.SM};
+			font-weight: ${props => props.theme.FONT.WEIGHT.MEDIUM};
+			color: ${props => props.theme.COLOR.PRIMARY};
+			text-align: left;
+		}
+
+		td {
 			width: 100%;
+			display: block;
+			padding-bottom: 8px;
 		}
 	}
 `;
@@ -39,14 +56,29 @@ export const Table = styled.table`
 export const ValueItem = styled.div`
 	padding: 16px 20px;
 
+	font-family: ${props => props.theme.FONT.SANS};
+	font-weight: ${props => props.theme.FONT.WEIGHT.REGULAR};
+	font-size: ${props => props.theme.FONT.SIZE.MD};
+	color: ${props => props.theme.COLOR.GRAY700};
+
 	b {
 		font-weight: ${props => props.theme.FONT.WEIGHT.SEMIBOLD};
+		color: ${props => props.theme.COLOR.PRIMARY};
+	}
+
+	@media ${props => props.theme.WINDOW_SIZE.mobile} {
+		padding: 12px 16px;
+		font-size: ${props => props.theme.FONT.SIZE.MD};
 	}
 `;
 
 export const RowSeparator = styled.hr`
-	width: 100%;
+	width: calc(100% - 30px);
+	margin: 0 auto;
 	border: none;
-	border-bottom: 1px solid ${props => props.theme.COLOR.GRAY200};
-	margin: 0;
+	border-bottom: 1px solid ${props => props.theme.COLOR.GRAY100};
+
+	@media ${props => props.theme.WINDOW_SIZE.mobile} {
+		width: 100%;
+	}
 `;
