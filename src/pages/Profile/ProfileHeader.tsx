6b6c940684e424d30f96e579adc38f7713e24456
조희우 <huiwoo.jo@gmail.com>
2026-02-23 07:15:24 +0900

    💄 change ProfileHeader in Profile to suit mobile environment

--- src/pages/Profile/ProfileHeader.tsx
+++ src/pages/Profile/ProfileHeader.tsx
@@ -1,8 +1,9 @@
 // @src/pages/Profile/components/ProfileHeader.tsx
 
 import * as S from '@styles/pages/Profile/ProfileHeader.style';
+
 import { PROFILE } from '@const/profile';
-import { GetSnsLabel, GetSnsUrl } from '@/utils/snsUrl';
+import { GetSnsLabel, GetSnsUrl } from '@utils/snsUrl';
 
 export const ProfileHeader = () => {
 	const { content, platform, account, contentTitle, postId } = PROFILE.description;
@@ -12,15 +13,13 @@ export const ProfileHeader = () => {
 			<S.MainImage src={PROFILE.mainImage} alt={PROFILE.name} />
 			<S.InfoSummary>
 				<S.NameGroup>
-					<h1>{PROFILE.name}</h1>
-					{PROFILE.enName}
-					<S.VerticalBar> | </S.VerticalBar>
-					{PROFILE.hanjaName}
+					<p className="name ">{PROFILE.name}</p>
+					<span className="sub-name">{PROFILE.enName}</span>
+					<span className="sub-name">{PROFILE.hanjaName}</span>
 				</S.NameGroup>
 
 				<S.Description>
 					{content}
-					<br />
 					<S.SourceLink
 						href={GetSnsUrl(platform, postId) ?? undefined}
 						$disabled={!GetSnsUrl(platform, postId)}
