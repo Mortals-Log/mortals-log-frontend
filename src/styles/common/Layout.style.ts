a044b59245eb8b3d4a5b353cf46d3b4af7efa125
조희우 <huiwoo.jo@gmail.com>
2026-02-23 08:02:28 +0900

    📝 add mobile-optimized sectionTitle to  layout style file

--- src/styles/common/Layout.style.ts
+++ src/styles/common/Layout.style.ts
@@ -23,3 +23,42 @@ export const ContentSection = styled.section`
 		margin-top: 4rem;
 	}
 `;
+
+export const SectionTitle = styled.h3`
+	display: flex;
+	align-items: baseline;
+	gap: 0.5rem;
+
+	font-family: ${props => props.theme.FONT.SERIF};
+	font-size: ${props => props.theme.FONT.SIZE.H3};
+	font-weight: ${props => props.theme.FONT.WEIGHT.REGULAR};
+	color: ${props => props.theme.COLOR.GRAY700};
+
+	border-bottom: 2px solid ${props => props.theme.COLOR.PRIMARY};
+	padding-bottom: 1rem;
+	letter-spacing: 2px;
+
+	flex-wrap: wrap;
+	word-break: keep-all;
+
+	span {
+		font-size: ${props => props.theme.FONT.SIZE.SM};
+		font-weight: ${props => props.theme.FONT.WEIGHT.REGULAR};
+		color: ${props => props.theme.COLOR.GRAY500};
+
+		text-transform: uppercase;
+		letter-spacing: 1.5px;
+	}
+
+	@media ${props => props.theme.WINDOW_SIZE.mobile} {
+		padding-bottom: 0.7rem;
+		letter-spacing: 1px;
+
+		gap: 0.4rem;
+
+		span {
+			font-size: ${props => props.theme.FONT.SIZE.XS};
+			letter-spacing: 1px;
+		}
+	}
+`;
