16a7fc02051ddf9598f72390e49944e2568b50b3
조희우 <huiwoo.jo@gmail.com>
2026-02-25 22:30:13 +0900

    ♻️ optimization ScheduleWeekView in ScheduleCalendar page

--- src/pages/ScheduleCalendar/ScheduleWeekView.tsx
+++ src/pages/ScheduleCalendar/ScheduleWeekView.tsx
@@ -1,8 +1,9 @@
-// @pages/ScheduleCalendar/WeekScheduleView.tsx
+// @/pages/ScheduleCalendar/WeekScheduleView
 
 import * as S from '@/styles/pages/ScheduleCalendar/ScheduleWeekView.style';
-
+import { useMemo, memo } from 'react';
 import { format, startOfWeek, addDays, isSameDay } from 'date-fns';
+import { ko } from 'date-fns/locale';
 import { Schedule } from '@/types/schedule';
 
 interface WeekScheduleViewProps {
@@ -13,27 +14,35 @@ interface WeekScheduleViewProps {
 }
 
 const ScheduleWeekView = ({ viewDate, selectedDate, onSelectDate, schedules }: WeekScheduleViewProps) => {
-	const startDate = startOfWeek(viewDate, { weekStartsOn: 1 });
-	const weekDays = Array.from({ length: 7 }, (_, i) => addDays(startDate, i));
+	const weekDays = useMemo(() => {
+		const startDate = startOfWeek(viewDate, { weekStartsOn: 0 });
+		return Array.from({ length: 7 }, (_, i) => addDays(startDate, i));
+	}, [viewDate]);
+
+	const today = new Date();
 
 	return (
 		<S.WeekContainer>
 			{weekDays.map(day => {
 				const dateKey = format(day, 'yyyy-MM-dd');
 				const daySchedules = schedules[dateKey] || [];
-				const isToday = isSameDay(day, new Date());
+				const isToday = isSameDay(day, today);
 				const isSelected = isSameDay(day, selectedDate);
 
 				return (
-					<S.DayContainer key={dateKey} isToday={isToday} isSelected={isSelected} onClick={() => onSelectDate(day)}>
-						<S.DayHeader isToday={isToday} isSelected={isSelected}>
-							<span className="day_name">{format(day, 'EEE')}</span>
+					<S.DayContainer key={dateKey} $isToday={isToday} $isSelected={isSelected} onClick={() => onSelectDate(day)}>
+						<S.DayHeader $isToday={isToday} $isSelected={isSelected}>
+							<span className="day_name">{format(day, 'EEE', { locale: ko })}</span>
 							<span className="day_number">{format(day, 'd')}</span>
 						</S.DayHeader>
 
 						<S.ScheduleList>
-							{daySchedules.map((sch, idx) => (
-								<S.ScheduleItem key={idx} eventType={sch.type} isSelected={isSelected} title={sch.content}>
+							{daySchedules.map(sch => (
+								<S.ScheduleItem
+									key={sch.id || `${dateKey}-${sch.content}`}
+									$eventType={sch.type}
+									$isSelected={isSelected}
+									title={sch.content}>
 									<span className="content">{sch.content}</span>
 								</S.ScheduleItem>
 							))}
@@ -45,4 +54,4 @@ const ScheduleWeekView = ({ viewDate, selectedDate, onSelectDate, schedules }: W
 	);
 };
 
-export default ScheduleWeekView;
+export default memo(ScheduleWeekView);
