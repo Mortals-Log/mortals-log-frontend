5b9a900c9c8f94302dbcfe7b564ad5e136d5ec9d
조희우 <huiwoo.jo@gmail.com>
2026-02-28 12:16:18 +0900

    💄 change Music to suit laptop environment

--- src/styles/pages/Music/Music.style.ts
+++ src/styles/pages/Music/Music.style.ts
@@ -1,4 +1,4 @@
-// @styles/pages/Music/Music.style
+// @/styles/pages/Music/Music.style
 
 /* eslint-disable storybook/default-exports */
 
@@ -17,7 +17,7 @@ export const TabGroup = styled.ul`
 	display: flex;
 	width: 100%;
 	align-items: center;
-	margin: 5rem 0 2rem 0;
+	margin: 4rem 0 1.5rem 0;
 
 	list-style: none;
 	overflow-x: auto;
@@ -30,12 +30,6 @@ export const TabGroup = styled.ul`
 	scrollbar-width: none;
 
 	@media ${props => props.theme.WINDOW_SIZE.tablet} {
-		margin: 4rem 0 1.5rem 0;
-		padding-bottom: 4px;
-	}
-
-	@media ${props => props.theme.WINDOW_SIZE.mobile} {
-		margin: 4rem 0 1.5rem 0;
 		padding-bottom: 4px;
 	}
 `;
@@ -59,28 +53,20 @@ export const TabItem = styled.li<{ $isActive: boolean }>`
 	background-color: ${props => (props.$isActive ? props.theme.COLOR.WHITE : props.theme.COLOR.GRAY100)};
 
 	font-family: ${props => props.theme.FONT.SERIF};
-	font-size: ${props => props.theme.FONT.SIZE.MD};
+	font-size: ${props => props.theme.FONT.SIZE.SM};
 	font-weight: ${props => props.theme.FONT.WEIGHT.SEMIBOLD};
 	color: ${props => (props.$isActive ? props.theme.COLOR.PRIMARY : props.theme.COLOR.GRAY400)};
 
-	&:hover {
-		text-decoration: underline;
-	}
-
-	@media ${props => props.theme.WINDOW_SIZE.tablet} {
-		font-size: ${props => props.theme.FONT.SIZE.SM};
-
-		&:hover {
-			text-decoration: none;
-		}
-	}
+	${props =>
+		!props.$isActive &&
+		`
+        &:hover {
+            background-color: ${props.theme.COLOR.GRAY200};
+            color: ${props.theme.COLOR.GRAY500};
+        }
+    `}
 
 	@media ${props => props.theme.WINDOW_SIZE.mobile} {
 		padding: 0.8rem 1rem;
-		font-size: ${props => props.theme.FONT.SIZE.SM};
-
-		&:hover {
-			text-decoration: none;
-		}
 	}
 `;
