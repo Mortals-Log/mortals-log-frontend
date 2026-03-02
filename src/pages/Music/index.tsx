5214ab42b24fad8428b80c6b831a5bce8614166b
조희우 <huiwoo.jo@gmail.com>
2026-03-02 13:25:39 +0900

    ✨ add meta information to Music

--- src/pages/Music/index.tsx
+++ src/pages/Music/index.tsx
@@ -2,10 +2,12 @@
 
 import * as S from '@/styles/pages/Music/Music.style';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Album from '@/pages/Album';
 import Song from '@/pages/Song';
 import { NAME } from '@/const/profile';
+import { METADATA } from '@/const/contents';
+import { UpdateMetaTags } from '@/utils/meta';
 
 const Music = () => {
 	const PAGE_TITLE = {
@@ -21,6 +23,17 @@ const Music = () => {
 	const handleTabClick = (tab: (typeof tabs)[number]) => {
 		setActiveTab(tab);
 	};
+
+	useEffect(() => {
+		const pageTitle = `${METADATA.NAME} | Music`;
+		const description = PAGE_TITLE.DESCRIPTION;
+
+		UpdateMetaTags(pageTitle, description, undefined, 'website');
+
+		return () => {
+			UpdateMetaTags(METADATA.NAME, METADATA.DESCRIPTION, undefined, 'website');
+		};
+	});
 
 	return (
 		<S.MainContainer>
