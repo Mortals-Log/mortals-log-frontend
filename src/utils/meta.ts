364c7d7f5a5024779b78e75e3a19a3258586579c
조희우 <huiwoo.jo@gmail.com>
2026-03-02 12:58:49 +0900

    ✨ add feature for URL meta information

---
+++ src/utils/meta.ts
@@ -0,0 +1,34 @@
+// @/utils/id
+
+/* eslint-disable storybook/default-exports */
+
+export const UpdateMetaTags = (
+	title: string,
+	description: string,
+	image: string,
+	type: 'website' | 'music.song' | 'music.album' = 'website',
+) => {
+	document.title = title;
+
+	const metaTags = [
+		{ property: 'og:title', content: title },
+		{ property: 'og:description', content: description },
+		{ property: 'og:image', content: image },
+		{ property: 'og:type', content: type },
+		{ name: 'twitter:card', content: 'summary_large_image' },
+	];
+
+	metaTags.forEach(({ property, name, content }) => {
+		let tag = property
+			? document.querySelector(`meta[property="${property}"]`)
+			: document.querySelector(`meta[name="${name}"]`);
+
+		if (!tag) {
+			tag = document.createElement('meta');
+			if (property) tag.setAttribute('property', property);
+			if (name) tag.setAttribute('name', name);
+			document.head.appendChild(tag);
+		}
+		tag.setAttribute('content', content);
+	});
+};
