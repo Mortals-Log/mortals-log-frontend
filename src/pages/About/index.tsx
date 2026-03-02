732177f45a199b8d22cb0055902a4e58063b4328
조희우 <huiwoo.jo@gmail.com>
2026-03-02 13:21:44 +0900

    ✨ add meta information to About

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,9 +2,12 @@
 
 import * as S from '@/styles/pages/About/About.style';
 
+import { useEffect } from 'react';
 import AboutTitle from '@/pages/About/AboutTitle';
 import AboutPolicySection from '@/pages/About/AboutPolicySection';
 import AboutInquirySection from '@/pages/About/AboutInquirySection';
+import { UpdateMetaTags } from '@/utils/meta';
+import { METADATA } from '@/const/contents';
 
 const SECTION_TITLE = {
 	POLICY: {
@@ -18,6 +21,17 @@ const SECTION_TITLE = {
 } as const;
 
 const About = () => {
+	useEffect(() => {
+		const pageTitle = `${METADATA.NAME} | About`;
+		const description = `${METADATA.NAME}의 저작권 정책 및 문의 정보를 확인하세요.`;
+
+		UpdateMetaTags(pageTitle, description, undefined, 'website');
+
+		return () => {
+			UpdateMetaTags(METADATA.NAME, METADATA.DESCRIPTION, undefined, 'website');
+		};
+	}, []);
+
 	return (
 		<S.MainContainer>
 			<AboutTitle />
